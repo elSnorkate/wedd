086224472a8cb6997fa684e5ccede59884379850
Srmiel <18mastarchll@gmail.com>
2023-11-17 19:26:02 -0500

    1.3.1

--- src/components/Invitaciones/02122301/EFC2.jsx
+++ src/components/Invitaciones/02122301/EFC2.jsx
@@ -12,18 +12,15 @@ export const EFC2 = () => {
         las piezas cobran sentido. 
       </p>
     </div> 
-    <div className='espejo'>
+    <div >
       <big className='bigI' style={{fontFamily:'Edu TAS Beginner', textDecoration:'underline dotted'}}>
           Nosotros nos hemos encontrado,   <br />
           Por eso...
       </big> <br />
-        <h2 className='kg' style={{color:'white', fontSize:'1000%'}}>!NOS CASAMOS¡</h2>
+        <h2 className='kg' style={{color:'white', fontSize:'990%'}}>!NOS CASAMOS¡</h2>
       <small className='smallS' style={{fontFamily:'Edu TAS Beginner' ,padding:'1rem',borderRadius:'1rem', backgroundColor:'white'}}>
         Y QUEREMOS QUE SEAS PARTE DE ESTE MOMENTO
-      </small>
-    </div>
-    <div >
-        <img src={Imagenes.Carro} alt="test drive" style={{width:'8rem',filter: 'drop-shadow(0.2rem 0.1rem 0.4em #ffffff)', }} id='carrito' />
+      </small><br /><br /><br />
     </div>
     </>
   )
--- src/components/Invitaciones/uwu/04/I41.jsx
+++ src/components/Invitaciones/uwu/04/I41.jsx
@@ -9,27 +9,25 @@ export const I41 = () => {
             <small>Con la bendicion de Dios y nuestros padres</small>
             <div className='a3'>
               <div className='normalBox'>
-                <img src={Imagenes.Madre} alt="madre" style={{height:'3rem'}} />
-                <h4>Madre del novio</h4>
-                <p> Fania Chuquillanqui Mendoza </p>
+                <img src={Imagenes.Madre} alt="madre" style={{height:'2rem'}} />
+                <h4 className='t1' >Madre del novio</h4>
+                <p  className='t2' > Fania Chuquillanqui M </p>
               </div>
               <div className='normalBox'>
-                <img src={Imagenes.Padres} alt="padres" style={{height:'3rem'}} />
-                <h4>Padres de la novia</h4>
-                <p>Juan Barreto Segura +<br /> Marta Tunqui Pari</p> <br />
+                <img src={Imagenes.Padres} alt="padres" style={{height:'2rem'}} />
+                <h4 className='t1' >Padres de la novia</h4>
+                <p  className='t2' >Juan Barreto S +<br /> Marta Tunqui Pari</p> <br />
               </div>
-            </div>
+            </div> <br /><br />
               <small>y asi tambien de nuestros padrinos:</small>
               <div id='a4' className='normalBox' style={{width:'70%', backgroundColor:'#f2d0bdc9'}}>
-                <img src={Imagenes.Casados} alt="padres" style={{height:'3rem'}} />
-                <div id='a5'>
-                <h4>Felizmente casados</h4>
+                <img src={Imagenes.Casados} alt="padres" style={{height:'2rem'}} />
+                <h4 style={{fontSize:'9px', marginBottom:'-5px'}}>Felizmente casados</h4>
                 <p>Meri Chuquillanqui Mendoza<br /> Isaac Zevallos Solis</p>
-                </div>
               </div>
-                <p>Es un placer invitarlos a nuestro gran dia</p>
-                <img src={Imagenes.Entrada} id='a6' alt="padres" style={{height:'3rem'}} /> <br /> 
-                <div className='frase' style={{height:'35px', backgroundColor:'#f2d0bd96',width:'90%', borderRadius:'1rem' }}> pase para 2 personas</div>
+                <p style={{fontSize:'80%'}}>Es un placer invitarlos a nuestro gran dia</p>
+                <img src={Imagenes.Entrada} id='a6' alt="padres" style={{height:'2.5rem'}} /> <br /> 
+                <div className='frase'  style={{height:'35px', background: 'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,208,189,1) 100%)', width:'90%'}}> pase para 2 personas</div>
               </div>
     </>
   )
